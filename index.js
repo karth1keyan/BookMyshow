228271b3a51daf9ebb6c5d892b10e7a222f9103e
karth1keyan <103503013+karth1keyan@users.noreply.github.com>
2023-03-13 00:19:58 +0530

    testing background

---
+++ index.js
@@ -0,0 +1,7 @@
+function func(exp){
+    if(exp.checked == true){
+        document.body.style.backgroundImage = "URL('Images/BLUEBG.jpg')" ;
+    }else{
+        document.body.style.backgroundImage = "URL('Images/REDBG.jpg')" ;
+    }
+}
