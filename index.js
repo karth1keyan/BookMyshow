40afeabb9190e493d0268299ec109ee274b4979d
karth1keyan <103503013+karth1keyan@users.noreply.github.com>
2023-04-24 04:05:08 +0530

    web app updated

--- index.js
+++
@@ -1,7 +0,0 @@
-function func(exp){
-    if(exp.checked == true){
-        document.body.style.backgroundImage = "URL('Images/BLUEBG.jpg')" ;
-    }else{
-        document.body.style.backgroundImage = "URL('Images/REDBG.jpg')" ;
-    }
-}
